c23b7b14cf1e6b02a028aeca8e1fcc68976e0ead
baek4070 <baek4070@gmail.com>
2021-10-08 16:20:36 +0900

    practice 수정

--- 국비지원학원/java/java_practice/src/java_practice_1008/ReserveApp.java
+++ 국비지원학원/java/java_practice/src/java_practice_1008/ReserveApp.java
@@ -5,49 +5,79 @@ import java.util.Scanner;
 public class ReserveApp {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[][] seat = new String[10][10];  // 좌석 배열  10x10 // index : [0 ~ 9,0 ~ 9]
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				seat[i][j] = "[□]";
-			}
-		}
-		char[] alpha = new char[10];		   // char type 배열 10개( X열 ) // index[0 ~ 9]
-		boolean isRun = true;                  // 최상단 while  조건식
-		while (isRun) {
-			System.out.println("[1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [행]");
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++) {
-					if(seat[i][j].equals("[■]")) {
-						System.out.print(seat[i][j]+" ");
-					}else {
-					seat[i][j] = "[□]";
-					System.out.print(seat[i][j]+" ");
-					}
+		/*for (byte[] b : seats) {
+			for(byte b1 : b) {
+				if(b1 == 0) {
+					System.out.print("[□]");
+				}else {
+					System.out.print("[■]");
 				}
-				alpha[i]= (char)(65+i) ;
-				System.out.print("["+alpha[i]+"열]");
-				System.out.println();
+				System.out.println("["+b1+"]");
 			}
-			System.out.println("예약해주실 좌석의 열을 입력해주세요>");
-			char input = sc.next().charAt(0); // 예약할 좌석의 열 
-			System.out.println("입력한 열 : "+input);
-			System.out.println("예약해주실 좌석의 행 번호를 입력해주세요>");
+			System.out.println();
+		}
+		 */
+		byte[][] seats = new byte[10][10];
+		Scanner sc = new Scanner(System.in);
+		char inputColumn = 0;
+		boolean isRun = true;
+		while (isRun) {
+			System.out.println("              [screen]");
+			for (int i = 0; i < seats.length; i++) {
+				System.out.print("["+(i+1)+"]");
+			}
+			System.out.println("[행]");
+			for (int i = 0; i < seats.length; i++) {
+				for (int j = 0; j < seats[i].length; j++) {
+					if(seats[i][j] == 0) {
+						System.out.print("[□]");
+					}else {
+						System.out.print("[■]");
+					}
+				}//j for 종료
+				System.out.println(" ["+(char)(i+65)+"열]");
+			}//i for 종료
+			System.out.println("예약하실 좌석의 열을 입력해주세요.");
+			String s = sc.next();
+			inputColumn = s.charAt(0);
+			if( (int)inputColumn >74 || (int)inputColumn <65) {
+				System.out.println("잘못된 값입니다 처음부터 진행하세요");
+				continue;
+			}
+			System.out.println("선택하신 열의 이름 : "+inputColumn);
+			System.out.println("예약하실 좌석의 행번호를 입력해 주세요.");
 			
-			int inputNum = sc.nextInt(); // 예약할 좌석의 행
-			System.out.println("선택하신 좌석은 : "+input+" 열 "+ inputNum+" 행입니다." );
+			boolean isCheck = sc.hasNextInt();
+			if(!isCheck) {
+				sc.next();
+				System.out.println("잘못된 값입니다 처음부터 진행하세요");
+				continue;
+			}
+			
+			int rowNum = sc.nextInt();
+			if( rowNum > 10 || rowNum < 1) {
+				System.out.println("잘못된 값입니다 처음부터 진행하세요");
+				continue;
+			}
+			
+			System.out.println("선택하신 좌석의 번호는 : " + inputColumn + "열" + rowNum + "행 입니다.");
 			System.out.println("예약 완료 하시겠습니까? y/n");
 			
 			char yesOrNo = sc.next().charAt(0); // Y/N 선택
 			
 			//예약 완료 했을 경우
 			if (yesOrNo == 'y' || yesOrNo == 'Y') {
-				if(seat[(int)input-65][inputNum-1].equals("[■]")) {
-					System.out.println("이미 예약 된 자리입니다. 다시선택해주세요");
+				if(seats[(int)inputColumn-65][rowNum-1] == 1) {
+					System.out.println("이미 예약 된 자리입니다. 예약을 취소 하시겠습니까? Y/N");
+					yesOrNo = sc.next().charAt(0);
+					if (yesOrNo == 'y' || yesOrNo == 'Y') {
+						seats[(int)inputColumn-65][rowNum-1] = 0;
+						System.out.println("예약 취소 되었습니다");
+					}
 					continue;
 				}
 				System.out.println("예약 완료 되었습니다");
-				seat[(int)input-65][inputNum-1] = "[■]";   // input : char 타입 형변환 int // A = 65 ~ 
+				seats[(int)inputColumn-65][rowNum-1] = 1;   // input : char -> int 형변환 // A = 65 ~ 
 				
 			// 그밖에 취소
 			}else { 
@@ -55,7 +85,59 @@ public class ReserveApp {
 				System.out.println("다시 선택해 주세요");
 				continue;
 			}
-		} // while 종료
+		}//while 종료
+//		Scanner sc = new Scanner(System.in);
+//		String[][] seat = new String[10][10];  // 좌석 배열  10x10 // index : [0 ~ 9,0 ~ 9]
+//		for (int i = 0; i < 10; i++) {
+//			for (int j = 0; j < 10; j++) {
+//				seat[i][j] = "[□]";
+//			}
+//		}
+//		char[] alpha = new char[10];		   // char type 배열 10개( X열 ) // index[0 ~ 9]
+//		boolean isRun = true;                  // 최상단 while  조건식
+//		while (isRun) {
+//			System.out.println("\t\t SCREEN ");
+//			System.out.println("[1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [행]");
+//			for (int i = 0; i < 10; i++) {
+//				for (int j = 0; j < 10; j++) {
+//					if(seat[i][j].equals("[■]")) {
+//						System.out.print(seat[i][j]+" ");
+//					}else {
+//					seat[i][j] = "[□]";
+//					System.out.print(seat[i][j]+" ");
+//					}
+//				}
+//				alpha[i]= (char)(65+i) ;
+//				System.out.print("["+alpha[i]+"열]");
+//				System.out.println();
+//			}
+//			System.out.println("예약해주실 좌석의 열을 입력해주세요>");
+//			char input = sc.next().charAt(0); // 예약할 좌석의 열 
+//			System.out.println("입력한 열 : "+input);
+//			System.out.println("예약해주실 좌석의 행 번호를 입력해주세요>");
+//			
+//			int inputNum = sc.nextInt(); // 예약할 좌석의 행
+//			System.out.println("선택하신 좌석은 : "+input+" 열 "+ inputNum+" 행입니다." );
+//			System.out.println("예약 완료 하시겠습니까? y/n");
+//			
+//			char yesOrNo = sc.next().charAt(0); // Y/N 선택
+//			
+//			//예약 완료 했을 경우
+//			if (yesOrNo == 'y' || yesOrNo == 'Y') {
+//				if(seat[(int)input-65][inputNum-1].equals("[■]")) {
+//					System.out.println("이미 예약 된 자리입니다. 다시선택해주세요");
+//					continue;
+//				}
+//				System.out.println("예약 완료 되었습니다");
+//				seat[(int)input-65][inputNum-1] = "[■]";   // input : char 타입 형변환 int // A = 65 ~ 
+//				
+//			// 그밖에 취소
+//			}else { 
+//				System.out.println("예약이 취소되었습니다");
+//				System.out.println("다시 선택해 주세요");
+//				continue;
+//			}
+//		} // while 종료
 	}// main 종료
 	
 }
--- 국비지원학원/java/java_practice/src/java_practice_1008/TheaterReserve.java
+++ 국비지원학원/java/java_practice/src/java_practice_1008/TheaterReserve.java
@@ -8,7 +8,7 @@ public class TheaterReserve {
 		final int size = 10;
 		int[] seats = new int[size];
 		Scanner scan = new Scanner(System.in);
-reserve:while (scan.hasNextInt()) {
+		while (true) {
 			System.out.println("---------------------------");
 			for (int i = 0; i < size; i++)
 				System.out.print(i+1 + " ");
@@ -18,23 +18,26 @@ reserve:while (scan.hasNextInt()) {
 			System.out.println("\n-----------------------------");
 			
 			System.out.print("원하시는 좌석번호를 입력하세요(종료는 -1): ");
-			int s = scan.nextInt(); // 좌석 입력
-			if (s == -1) {
-				System.out.println("종료합니다");
-				break reserve;
-			}
-			if (s > 10 ) {
-				System.out.println("잘못된 값입니다");
+			
+			//문자 인지  정수인지  체크 하고 문자일경우 다시 실행하는 로직
+			boolean isCheck = scan.hasNextInt(); // 무조건 얘가 28번 라인보다 위에 있어야 함
+			if(!isCheck) {
+				scan.next(); // scan에 값이 남아 있기때문에 여기서 한번 뱉어줘야 됨  
+				continue;  // 안그러고 continue 하면 계속 무한반복 continue 합니다
+			}//문자 인지  정수인지  체크 하고 문자일경우 다시 실행하는 로직
+			int seatNum = scan.nextInt(); // 좌석 입력
+			if (seatNum == -1) {
+				System.out.println("시스텝이 종료되었습니다");
+				break; // break , continue 의 범위 확실히 공부할것 !! 
+			}else if (seatNum > 10 || seatNum < 1) {
+				System.out.println("선택할 수 없는 자리입니다");
 				continue;
-			}
-			if (seats[s-1] == 1) {
+			}else if (seats[seatNum-1] == 1) {
 				System.out.println("이미 예약된 좌석입니다");
 				continue;
 			}
-			seats[s-1] = 1;
+			seats[seatNum-1] = 1;
 			System.out.println("예약 되었습니다");
-		}
-		System.out.println("잘못된 값입니다 종료합니다");
-	}
-
-}
+		}//while 종료
+	}//main 종료
+}//class 종료
