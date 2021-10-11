cb06f6c76c6c8685435c916848d01545d302dc1e
baek4070 <baek4070@gmail.com>
2021-10-11 10:35:51 +0900

    class 선언

---
+++ 국비지원학원/java/08_class/src/a/declaration/Student.java
@@ -0,0 +1,15 @@
+package a.declaration;
+
+// class 선언부
+// 		   (여기 이름 부분)이 .자바파일과 일치해야한다 (뭐 어차피 자동으로 되는데?)
+public class Student {
+
+
+
+}
+//하나의 파일안에 여러개의 클래스 선언가능(하지만 권장x) // public은 하나만 정의가능(파일이름과일치)
+class Bus{
+	
+}
+//이대로 저장하면 2개의 class파일이 생성될것
+//C:\til\국비지원학원\java\08_class\bin\a\declaration
---
+++ 국비지원학원/java/08_class/src/a/declaration/StudentExample.java
@@ -0,0 +1,21 @@
+package a.declaration;
+
+
+//main method 가 존재하는 실행 class
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student s1; //선언
+		s1 = new Student();
+		System.out.println("Student class 를 참조하여 s1 객체 생성" + s1);
+		Student s2 = new Student();
+		System.out.println("Student class 를 참조하여 s2 객체 생성" + s2);
+		//s1, s2에 생성된 주소값이 저장됨 
+		Bus bus = null;
+//		bus = new Student(); 참조할수 없다
+		bus = new Bus();
+		System.out.println(bus);
+		System.out.println(s1 == s2);
+	}
+
+}
