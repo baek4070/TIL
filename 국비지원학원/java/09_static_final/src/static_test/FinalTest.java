6aeb1caa4e277ab74dbc65564f63b01ad0db79a1
baek4070 <baek4070@gmail.com>
2021-10-13 16:29:03 +0900

    접근제한자

--- 국비지원학원/java/09_static_final/src/static_test/FinalTest.java
+++ 국비지원학원/java/09_static_final/src/static_test/FinalTest.java
@@ -8,6 +8,12 @@ public class FinalTest {
 	
 	final int c;
 	
+	// 값이 변경되면 안되면서 동시에 어디서도 사용이 가능한
+	// 아래 녀석들을 상수 라고 합니다
+	// 키워드 순서는 바뀌어도 상관이 x 
+	static final double PI = 3.14; 		
+	final static int MAX_NUMBER = 25;	
+	
 	FinalTest() {
 		c = 100; //이라인을 주석처리하면 초기화 되지 않은 파이널 필드가 존재한다고 알려준다
 	}
@@ -24,6 +30,12 @@ public class FinalTest {
 		System.out.println(v);
 		//v = 101;  //값 변경불가 ~ 
 		final int b = 100;
+		System.out.println(b);
+		System.out.println(PI);
+		System.out.println(FinalTest.PI);
+//		Math.PI = 100; // 당연히 안됩니다 
+		System.out.println(Math.PI);
+		//static int a;
 	}
 	
 }
---
+++ 국비지원학원/java/10_access_modifier/src/a2/C.java
@@ -0,0 +1,8 @@
+package a2;
+//import a1.B
+
+public class C {
+// A a = new A(); a1은 퍼블릭이라 어디서든 가능하지만 이렇게 작성은아니고
+ a1.A a = new a1.A(); //이렇게 작성하도록
+// a1.B b = new a1.B(); // 이렇게 사용을 못한다 // 접근제한 
+}
