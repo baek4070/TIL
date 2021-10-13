aed740de79363f55e02fedb390b997a878f99f69
baek4070 <baek4070@gmail.com>
2021-10-13 12:39:06 +0900

    java_class

---
+++ 국비지원학원/java/08_class/src/f/cooperation/TakeTrans.java
@@ -0,0 +1,27 @@
+package f.cooperation;
+
+public class TakeTrans {
+
+	public static void main(String[] args) {
+		Bus bus100 = new Bus(100);
+		bus100.showInfo();
+		
+		Subway subwayGreen = new Subway("2호선");
+		subwayGreen.showInfo();
+		
+		Student studentSim = new Student("sim", 5000);
+		Student studentchoi = new Student("choi", 100000000);
+		
+		studentSim.takeBus(bus100);
+		
+		studentchoi.takeSubway(subwayGreen);
+		
+		bus100.showInfo();
+		studentSim.showInfo();
+		
+		subwayGreen.showInfo();
+		studentchoi.showInfo();
+	}
+	
+
+}
