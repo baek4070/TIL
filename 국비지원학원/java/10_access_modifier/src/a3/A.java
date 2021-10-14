e5375a1a83c4ae55c41cc8bd35b9e6a7eecee02e
baek4070 <baek4070@gmail.com>
2021-10-14 09:11:47 +0900

    java 접근제한자

--- 국비지원학원/java/10_access_modifier/src/a3/A.java
+++ 국비지원학원/java/10_access_modifier/src/a3/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	A a = new A();
 	A a1 = new A(10);
-	A a2 = new A("이기적인 놈")
+	A a2 = new A("이기적인 놈");
 			
 	public A() {}
 	A(int a){}
--- 국비지원학원/java/10_access_modifier/src/a3/B.java
+++ 국비지원학원/java/10_access_modifier/src/a3/B.java
@@ -3,5 +3,5 @@ package a3;
 public class B {
 	A a = new A();		//public
 	A a1 = new A(1);	//default
-	A a2 = new A("이기적인 놈"); //private
+//	A a2 = new A("이기적인 놈"); //private
 }
