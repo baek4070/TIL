b402878c86e8e2ee91047f7497f73accf4ebf231
baek4070 <baek4070@gmail.com>
2021-10-21 12:36:38 +0900

    Java API class_object

--- 국비지원학원/java/16_API_class/src/a/object/clone/CloneExample.java
+++ 국비지원학원/java/16_API_class/src/a/object/clone/CloneExample.java
@@ -12,6 +12,15 @@ public class CloneExample {
 		System.out.println(member);
 		Member cloned = member.getMember();
 		System.out.println(cloned);
+		System.out.println(member.hashCode());// 주소값이 다른거 확인
+		System.out.println(cloned.hashCode());//
+		
+		cloned.score[0] = 90; // 복제된 객체의 필드값 변경
+		cloned.car.model ="소나타";
+		cloned.name="심현석";
+		
+		System.out.println(cloned); // 복제된 객체의 값을 변경했는데 원본 필드값도 변경되었음
+		System.out.println(member); // 주소값을 복제 저장하기떄문에 같은곳을 참조하고 있기때문
 	}
 
 }
--- 국비지원학원/java/16_API_class/src/a/object/clone/Member.java
+++ 국비지원학원/java/16_API_class/src/a/object/clone/Member.java
@@ -1,5 +1,8 @@
 package a.object.clone;
-					// Cloneable 은 기능이 하나도 없음 클론 허용용 마크용 인터페이스
+
+import java.util.Arrays;
+
+// Cloneable 은 기능이 하나도 없음 클론 허용용 마크용 인터페이스
 public class Member implements Cloneable{
 
 	public String name;
@@ -20,19 +23,36 @@ public class Member implements Cloneable{
 
 	@Override
 	public String toString() {
-		return "Member [name=" + name + ", age=" + age + ", score=" + score + ", car=" + car + "]";
+		return "Member [name=" + name + ", age=" + age + ", score=" + Arrays.toString(score) + ", car=" + car + "]";
 	}
+	
+	
 	
 	public Member getMember() {
 		Member cloned = null;
 		try {
 			
-			cloned = (Member)super.clone();
+//			cloned = (Member)super.clone();
+			cloned = (Member)clone();
 			
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
 		return cloned;
 	}
-	
+	//깊은 복제 
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Member cloned = (Member)super.clone();
+		int[] score = new int[this.score.length]; // 원본 객체의 배열의 길이만큼 새로운 배열 생성
+		for (int i = 0; i < score.length; i++) {
+			score[i] = this.score[i];
+		}
+		cloned.score = score;
+		Car car = new Car(this.car.model); // 원본 객체의 Car값과 똑같은 새로은 Car필드 생성
+		cloned.car = car;
+		return cloned;
+	}
+
+
 }
