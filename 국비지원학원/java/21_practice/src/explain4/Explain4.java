fb7daaa1db9f98392e1d12259fa975ed3ea13a05
baek4070 <baek4070@gmail.com>
2021-10-27 17:24:46 +0900

    java_colletion_plactice

---
+++ 국비지원학원/java/21_practice/src/explain4/Explain4.java
@@ -0,0 +1,26 @@
+package explain4;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Explain4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		TreeSet<Integer> set = new TreeSet<>();
+		System.out.println("정수(-1 이 될때까지 입력)");
+		while (true) {
+			int n = sc.nextInt();
+			if(n == -1) break;
+			set.add(n);
+		}
+		int min = set.first();
+		int max = set.last();
+		System.out.println(set);
+		System.out.println("최소값 : " + min);
+		System.out.println("최대값 : " + max);
+		System.out.println("시스템 종료");
+		sc.close();
+	}
+
+}
