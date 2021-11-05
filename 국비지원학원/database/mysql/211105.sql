bb438536a864cf5943ba137aa44f2b29be45dd5d
baek4070 <baek4070@gmail.com>
2021-11-05 12:37:28 +0900

    MySQL index

--- 국비지원학원/database/mysql/211105.sql
+++ 국비지원학원/database/mysql/211105.sql
@@ -47,7 +47,8 @@ SELECT userTbl.name,
 FROM userTbl INNER JOIN buyTbl
 ON userTbl.userID=buyTbl.userID
 WHERE buyTbl.Groupname = '전자';
--- BATURL 변경
+
+-- NATURL 변경
 SELECT userTbl.name,
 		userTbl.birthYear,
         userTbl.mobile1,
@@ -55,6 +56,29 @@ SELECT userTbl.name,
         , buyTbl.prodname
 FROM userTbl NATURAL JOIN buyTbl
 WHERE buyTbl.Groupname = '전자';
+
+-- '전자' 그룹 물품을 구매한 사용자의 
+-- 이름, 출생년도, 전화번호, 상품그룹이름, 상품이름 검색
+
+SELECT 
+	u.name, 
+    u.birthYear, 
+    concat(u.mobile1,u.mobile2) AS 전화번호,
+    b.groupName,
+    b.prodName
+FROM userTbl AS u JOIN buyTbl AS b 
+ON u.userID = b.userID 
+AND b.groupName = '전자';
+
+-- 위의 내용을 NATURAL JOIN 으로 변경
+SELECT 
+	u.name, 
+    u.birthYear, 
+    concat(u.mobile1,u.mobile2) AS 전화번호,
+    b.groupName,
+    b.prodName
+FROM userTbl AS u NATURAL JOIN buyTbl AS b
+WHERE b.groupName = '전자';
 
 /*****************************************************/
 -- VIEW 활용하여 검색 결과에 대한 
@@ -100,6 +124,7 @@ FROM
 	FROM emp
 	GROUP BY deptno) tmp, salgrade s
 WHERE avgSal BETWEEN s.losal AND s.hisal;
+
 -- 부서별 평균 급여와 급여 등급을 출력 - 인라인 뷰 이용
 -- 부서이름, 평균급여, 급여등급 형식으로 출력
 SELECT d.dname, avgSal, grade
@@ -119,4 +144,48 @@ WHERE e.sal BETWEEN s.losal AND s.hisal
 GROUP BY s.grade;
 
 
+/***********************************************************************************************/
+-- INDEX - 색인 , 목록
+-- Data 를 찾긱 위한 데이터
+-- 책의 목차에 비유
+-- 특정 테이블의 특정 컬럼(열)을 지정하여 인덱스를 생성
+-- 지정한 인덱스를 이용하여 데이터를 검색
+-- 목적은 빠른 데이터 검색
+-- 기본키(PRIMARY KEY)를 지정한 컬럼에 대해서는
+-- 자동으로 INDEX가 생성:생성된 기본키를 기준으로 순서대로 정렬
+-- 나머지 컬럼(열)이 검색 조건이 된다면
+-- 직접 인데스를 생셩해서 사용
 
+
+-- CREATE INDEX 인덱스 이름 ON 테이블이름(컬럼이름);
+-- ALTER TABLE 테이블 이름 ADD INDEX 인덱스 이름(컬럼이름)
+-- emp 테이블의 sal열에 isx_emp_sal 이름의 인덱스 부여
+ALTER TABLE emp ADD INDEX idx_emp_sal(sal);
+CREATE INDEX inx_emp_sal ON emp(sal);
+
+-- emp 테이블에 적용된 인덱스 정보 확인
+SHOW INDEXES FROM emp;
+-- 인덱스 삭제
+-- DROP INDEX 인덱스 이름 ON table이름.column이름;
+-- ALTER TABLE 테이블 이름 DROP INDEX 인덱스 이름;
+ALTER TABLE emp DROP INDEX idx_emp_sal;
+DROP INDEX inx_emp_sal ON smartWeb.emp;
+
+use employees;
+SHOW tables;
+SELECT count(*) FROM employees;
+DESC employees;
+
+SELECT * FROM employees 
+WHERE gender = 'M';
+CREATE INDEX idx_emp_gender ON employees(gender);
+DROP INDEX idx_emp_gender ON employees.employees;
+SHOW INDEXES FROM employees;
+
+-- 인덱스는 몯든 컬럼에 생성하는것이 좋지 않음.
+-- 저장 공간 차지
+-- 인덱스가 생성된 컬럼에 데이터의 삽입/삭제/수정이 일어나면
+-- 인덱스 페이지를 새로 생성해야 할 수 있어서 성능에 악영향
+-- 검색에 자주 사용되는 컬럼에만 생성
+-- 데이터 변경이 자주 일어나지 않는 테이블에 생성
+-- 검색쿼리에 인덱스는 영향을 받는다.
