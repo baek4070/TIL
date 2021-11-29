f5e080d7e0098c1bd48da9ae5ee9e0699270f447
baek4070 <baek4070@gmail.com>
2021-11-29 14:15:33 +0900

    teacher project

---
+++ 국비지원학원/javafx/push_man_client/src/push_man/main/MainThread.java
@@ -0,0 +1,10 @@
+package push_man.main;
+
+public class MainThread extends Thread {
+
+	//server 에서 발신한 내용을 Receive 
+	@Override
+	public void run() {
+		
+	}
+}
---
+++ 국비지원학원/javafx/push_man_server/pushman.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS push_man_member;
+
+CREATE TABLE IF NOT EXISTS push_man_member(
+	memberNum int(11) PRIMARY KEY AUTO_INCREMENT,
+	memberName VARCHAR(50) DEFAULT null,
+	memberId VARCHAR(100) UNIQUE NOT NULL,
+	memberPw VARCHAR(100) NOT NULL,
+	regadata BIGINT DEFAULT 0
+);
